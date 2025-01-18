e8c3799b893bff89be7271dbe4fa95b8d2606520
KimBergstroem <kimmen_b@hotmail.com>
2025-01-18 15:02:30 +0100

    foo: this is will fail

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,79 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "node:url"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer"
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  assetsInclude: ["**/*.PNG", "**/*.png"],
-});
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      png: {
+        quality: 80,
+      },
+      jpeg: {
+        quality: 80,
+      },
+      jpg: {
+        quality: 80,
+      },
+      webp: {
+        quality: 80,
+        lossless: false,
+      },
+    }),
+  ],
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    ],
+  },
+  build: {
+    minify: "esbuild",
+    target: "es2020",
+    cssMinify: true,
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (
+            id.includes("node_modules/react/") ||
+            id.includes("node_modules/react-dom/")
+          ) {
+            return "react-vendor"
+          }
+          if (id.includes("node_modules/framer-motion")) {
+            return "animation-vendor"
+          }
+          if (id.includes("/components/animation/")) {
+            return "animation-components"
+          }
+          if (id.includes("/components/sections/")) {
+            return "section-components"
+          }
+        },
+        entryFileNames: "assets/[name]-[hash].js",
+        chunkFileNames: "assets/[name]-[hash].js",
+        assetFileNames: "assets/[name]-[hash].[ext]",
+      },
+    },
+  },
+  server: {
+    port: 3000,
+    open: true,
+    hmr: {
+      overlay: true,
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      minifyWhitespace: true,
+      minifyIdentifiers: true,
+      minifySyntax: true,
+      drop: ["console", "debugger"],
+      legalComments: "none",
+    },
+    include: ["react", "react-dom", "framer-motion"],
+  },
+})
